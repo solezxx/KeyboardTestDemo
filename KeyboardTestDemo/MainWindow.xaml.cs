02af518e630beaa791fe8286544dc010e2d97533
zxx <357133157@qq.com>
2023-02-22 22:24:45 +0800

    增加校验码

--- KeyboardTestDemo/MainWindow.xaml.cs
+++ KeyboardTestDemo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.IO.Ports;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -25,7 +26,6 @@ namespace KeyboardTestDemo
         {
             await Task.Run((() =>
             {
-                Console.WriteLine(1111);
                 string[] oldPortNames = SerialPort.GetPortNames(); // 记录当前可用的串口
                 while (true)
                 {
@@ -66,7 +66,7 @@ namespace KeyboardTestDemo
         }
 
         private string dataBuffer = "";
-        public int count=0;
+        public int count = 0;
 
         private void M3_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
@@ -140,45 +140,52 @@ namespace KeyboardTestDemo
                                     TextBox1.AppendText($"TP按下，位置{LabelX.Content}{LabelY.Content}\r\n");
                                 }
                             }
-                            //----------------------------------------------------------
+                            //-------------------------------------------------------------------------------------------
                             //链路建立响应消息
                             if (completeData.Contains("3A0D010101"))
                             {
-                                if (CheckBox.IsChecked == true)
-                                {
-                                    WriteToM3("5A 5A 5A 5A 0D 00 00 00 00 00 00 00 3A 0D 01 01 02 11 1C 0A 0D");
-                                }
-                                else if (CheckBox.IsChecked == false)
-                                {
-                                    WriteToM3("5A5A5A5A190000000000000001070301000701000901000F01000C0100A4CF0A0D");
-                                }
+                                WriteToM3("5A 5A 5A 5A 0D 00 00 00 00 00 00 00 3A 0D 01 01 02 11 1C 0A 0D");
                             }
-                            //查询加锁状态
+                            //查询SN
                             if (completeData.Contains("3A0101F5"))
                             {
                                 count++;
+                                SN = SNKey="";
                                 if (count == 3)
                                 {
-                                    //WriteToM3("5A5A5A5A0C0000000000000014050301A8690A0D");
-                                    WriteToM3("5A 5A 5A 5A 0C 00 00 00 00 00 00 00 14 01 01 01 0A D3 0A 0D");
+                                    WriteToM3("5A 5A 5A 5A 0C 00 00 00 17 00 00 00 14 05 02 04 90 F0 0A 0D ");
                                     count = 0;
                                 }
                             }
+                            //获取SN,查询加锁状态
+                            if (completeData.Contains("1405020400"))
+                            {
+                                var indexsn = completeData.IndexOf("1405020400");
+                                SNKey = completeData.Substring(indexsn + 10, completeData.Length - indexsn - 18);
+                                SN = AscToString(SNKey);
+                                TextBox1.AppendText($"SN:{SN}");
+                                WriteToM3("5A 5A 5A 5A 0C 00 00 00 18 00 00 00 14 05 03 01 C6 4A 0A 0D");
+                            }
 
-                            //解锁消息
-                            //if (completeData.Contains("14050301"))
-                            //{
-                            //    if (completeData.Contains("140503010000"))
-                            //    {
-                            //        WriteToM3("5A5A5A5A190000000000000001070301000701000901000F01000C0100A4CF0A0D");
-                            //    }
-                            //    else
-                            //    {
-                            //        //WriteToM3("5A5A5A5A0C0000000000000014050301A8690A0D");
-                            //        WriteToM3("5A 5A 5A 5A 0C 00 00 00 00 00 00 00 14 01 01 01 0A D3 0A 0D");
-                            //    }
-                            //}
 
+                            //查询加锁状态成功，判断是否加锁
+                            if (completeData.Contains("1405030100"))
+                            {
+                                var l = completeData.IndexOf("1405030100");
+                                if (completeData.Substring(l + 10, 2) == "00")//无锁
+                                {
+                                    //解锁消息
+                                    WriteToM3("5A 5A 5A 5A 19 00 00 00 22 00 00 00 01 07 03 01 00 07 01 00 09 01 00 0F 01 00 0C 01 00 D2 79 0A 0D");
+                                }
+                                else
+                                {
+                                    String input = "1C0000002300000014050303"+SNKey;
+                                    var a = StringToHexBytes(input);
+                                    CRCXMODEM(a,a.Length);
+                                    string s = "5A 5A 5A 5A 1C 00 00 00 23 00 00 00 14 05 03 03" + SNKey  + "AF670A0D";
+                                    WriteToM3(s);
+                                }
+                            }
                             TextBox2.AppendText(completeData.Replace("0A0D", "0A0D\r\n"));
                         }));
 
@@ -193,6 +200,8 @@ namespace KeyboardTestDemo
         }
 
         public SerialPort M3;
+        private string SN, SNKey;
+        private StringBuilder sb;
 
         private void MainWindow_OnClosed(object sender, EventArgs e)
         {
@@ -204,12 +213,6 @@ namespace KeyboardTestDemo
         {
             TextBox1.Text = "";
             TextBox2.Text = "";
-        }
-
-        private void SendMsg1_Click(object sender, RoutedEventArgs e)
-        {
-            byte[] data = { 0x5A, 0x5A, 0x5A, 0x5A, 0x19, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x07, 0x03, 0x01, 0x00, 0x07, 0x01, 0x00, 0x09, 0x01, 0x00, 0x0F, 0x01, 0x00, 0x0C, 0x01, 0x00, 0xA4, 0xCF, 0x0A, 0x0D };
-            M3.Write(data, 0, data.Length); // 发送数据
         }
 
         private void TextBox_OnTextChanged(object sender, TextChangedEventArgs e)
@@ -254,15 +257,21 @@ namespace KeyboardTestDemo
             }
         }
 
+        public byte[] StringToHexBytes(string str)
+        {
+            str = str.Replace(" ", "");
+            int NumberChars = str.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(str.Substring(i, 2), 16);
+            return bytes;
+        }
+
         public bool WriteToM3(string hex)
         {
             try
             {
-                hex = hex.Replace(" ", "");
-                int NumberChars = hex.Length;
-                byte[] bytes = new byte[NumberChars / 2];
-                for (int i = 0; i < NumberChars; i += 2)
-                    bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+                var bytes = StringToHexBytes(hex);
                 if (M3 != null)
                 {
                     M3.Write(bytes, 0, bytes.Length); // 发送数据
@@ -275,12 +284,71 @@ namespace KeyboardTestDemo
             }
         }
 
+        public string AscToString(string asc)
+        {
+            try
+            {
+                sb = new StringBuilder();
+                for (int i = 0; i < asc.Length; i += 2)
+                {
+                    string hexChar = asc.Substring(i, 2);
+                    int charValue = Convert.ToInt32(hexChar, 16);
+                    sb.Append(Convert.ToChar(charValue));
+                }
+            }
+            catch (Exception e)
+            {
+             TextBox1.AppendText(e.Message+"\r\n");
+            }
+            return sb.ToString();
+        }
+        
         private void DisConnect_Click(object sender, RoutedEventArgs e)
         {
-            if (M3!=null)
+            if (M3 != null)
             {
                 M3.Close();
             }
+        }
+
+        public static UInt16 CRCXMODEM(byte[] data, int size)
+        {
+            UInt32 i = 0;
+            UInt16 crc = 0;
+            for (i = 0; i < size; i++)
+            {
+                crc = UpdateCRC16(crc, data[i]);
+            }
+            crc = UpdateCRC16(crc, 0);
+            crc = UpdateCRC16(crc, 0);
+            return (UInt16)(crc);
+        }
+
+        /// <summary>
+        /// 更新RCR16校验
+        /// </summary>
+        /// <param name="crcIn"></param>
+        /// <param name="bytee"></param>
+        /// <returns></returns>
+        private static UInt16 UpdateCRC16(UInt16 crcIn, byte bytee)
+        {
+            UInt32 crc = crcIn;
+            UInt32 ins = (UInt32)bytee | 0x100;
+            do
+            {
+                crc <<= 1;
+                ins <<= 1;
+                if ((ins & 0x100) == 0x100)
+                {
+                    ++crc;
+                }
+                if ((crc & 0x10000) == 0x10000)
+                {
+                    crc ^= 0x1021;
+                }
+            }
+            while (!((ins & 0x10000) == 0x10000));
+            return (UInt16)crc;
         }
     }
 }
